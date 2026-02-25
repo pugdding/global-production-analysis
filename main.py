93d047297cff973d1f4dedc0f84d98289fc16437
jasmyne nguyen <jasmynernguyen@gmail.com>
2026-02-25 13:37:17 -0800

    split data and train linear regression model

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ import numpy as np
 import seaborn as sb
 import matplotlib.pyplot as plt 
 
+#Import Scikit-Learn For Machine Learning
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split 
+
+
 #Looking For Datasets On Kaggle
 dataset=pd.read_csv("globalPlasticProduction.csv")
 
@@ -12,7 +17,7 @@ print(dataset.head())
 print(dataset.dtypes)
 print(dataset.shape)
 
-#Question -- how has the annual growth rate changed of global plastic pollution changed overtime?
+#Question -- how has the annual growth rtate changed of global plastic pollution changed overtime?
 
 #Analysis
 
@@ -39,6 +44,30 @@ plt.ylabel("Blobal Plastic Pollution Per Million Tons")
 plt.show()
 
 #Introduction To Machine Learning 
+
+#Get The Data 
+x=dataset[["Year"]]
+y=dataset[["Plastic"]]
+
+#Split The Data Into Training and Testing 
+xTrain,xTest,yTrain,yTest=train_test_split(x,y,test_size=0.2,random_state=42)
+
+#Creating The Model and Training
+model=LinearRegression()
+model.fit(xTrain,yTrain)
+prediction=model.predict(xTest)
+
+#Create The Chart
+plt.figure()
+plt.scatter(xTest, yTest, color="blue", label="Actual")
+plt.plot(xTest, prediction, color="red", label="Predicted")
+plt.title("Actual vs Predicted Plastic Production")
+plt.xlabel("Year")
+plt.ylabel("Plastic Production")
+plt.legend()
+plt.show()
+
+
 
 
 
